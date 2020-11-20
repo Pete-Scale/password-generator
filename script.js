8eefbc93024f111040a199b7d0635c171526b921
Pete-Scale <petescale@gmail.com>
2020-11-20 13:17:22 -0600

    switched to ascii codes to include all spec chars need a gen array function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var lowCaseCharCodes = generateArray(65, 90)
 
-var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var upCaseCharCodes = generateArray(97, 122)
 
-var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numCharCodes = generateArray(48, 57)
 
-var specialChar = [];
+var specCharCodes = generateArray(33, 47).concat(generateArray(58, 64)).concat(generateArray(91, 96)).concat(generateArray(123, 126))
 
 
 // Write password to the #password input
