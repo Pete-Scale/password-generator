aa1eee2a2d900198df104b70bb58f2917e0958a4
Pete-Scale <petescale@gmail.com>
2020-11-20 17:48:44 -0600

    made prompt for length and defined generatePassword

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ var numCharCodes = generateArray(48, 57)
 
 var specCharCodes = generateArray(33, 47).concat(generateArray(58, 64)).concat(generateArray(91, 96)).concat(generateArray(123, 126))
 
+// Prompt for user typing in a number between 8-128
+function chooseLength(){
+  parseInt(prompt('Choose password length between 8 and 128 characters'))
+}
+
+// Confirm for user choosing types of characters to include
+
+
+// Generates array full of numbers from ascii code 
 function generateArray(low, high) {
   var charArray = []
   for (var i = low; i <= high; i++) {
@@ -17,20 +26,31 @@ function generateArray(low, high) {
   return charArray
 }
 
-console.log(lowCaseCharCodes)
-console.log(upCaseCharCodes)
-console.log(numCharCodes)
-console.log(specCharCodes)
+// Translate array of ascii code numbers to characters
+
+
+// Combine user choices to generate password
+function generatePassword(){
+
+  chooseLength()
+}
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
+// generateBtn.addEventListener("click", passwordLength);
 generateBtn.addEventListener("click", writePassword);
+
+// Console log check
+console.log(lowCaseCharCodes)
+console.log(upCaseCharCodes)
+console.log(numCharCodes)
+console.log(specCharCodes)
