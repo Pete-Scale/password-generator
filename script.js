6033f5329efc7d210e41cdebdcf6e4c17020ce94
Pete-Scale <petescale@gmail.com>
2020-11-21 15:11:13 -0600

    fully updated my comments in js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// ASCII assignment codes
 var lowCaseCharCodes = generateArray(97, 122)
 
 var upCaseCharCodes = generateArray(65, 90)
@@ -9,10 +10,16 @@ var numCharCodes = generateArray(48, 57)
 
 var specCharCodes = generateArray(33, 47).concat(generateArray(58, 64)).concat(generateArray(91, 96)).concat(generateArray(123, 126))
 
+// // Console log check
+// console.log(lowCaseCharCodes)
+// console.log(upCaseCharCodes)
+// console.log(numCharCodes)
+// console.log(specCharCodes)
+
 // Prompt for user typing in a number between 8-128
 
 
-// Generates array full of numbers from ascii code 
+// Generates array full of numbers from ascii codes from low to high 
 function generateArray(low, high) {
   var charArray = []
   for (var i = low; i <= high; i++) {
@@ -45,8 +52,11 @@ function generatePassword(){
   // Confirm for user including special characters
   var includeSpecChars = confirm('Include special characters?') 
 
+  // Reload page if user doesn't choose any chars
   if ((includeLowCase === false && includeUpCase === false) && (includeNums === false && includeSpecChars === false)) {
     location = location;
+
+    // Add user char choices to the final char array
   } else {
     var finalCharArray = []
     if (includeLowCase === true) {
@@ -61,38 +71,33 @@ function generatePassword(){
     if (includeSpecChars === true) {
       finalCharArray = finalCharArray.concat(specCharCodes)
     }
+    
+    // Generate the password array
     var passwordArray = []
     for (var i = 0; i < lengthChoice; i++){
+      // Randomize the final char array by the length of the char options
       var eachChar = finalCharArray[Math.floor(Math.random() * finalCharArray.length)]
+      // Convert the random array char codes to proper string values
       passwordArray.push(String.fromCharCode(eachChar))
     }
+    // Remove commas in password string
     return passwordArray.join('')
   }
-
-    console.log(lengthChoice)
-    console.log(includeLowCase)
-    console.log(includeUpCase)
-    console.log(includeNums)
-    console.log(includeSpecChars)
-
+    // // Console log check
+    // console.log(lengthChoice)
+    // console.log(includeLowCase)
+    // console.log(includeUpCase)
+    // console.log(includeNums)
+    // console.log(includeSpecChars)
 }
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
+  var passwordText = document.querySelector("#password"); 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", passwordLength);
 generateBtn.addEventListener("click", writePassword);
-
-// Console log check
-console.log(lowCaseCharCodes)
-console.log(upCaseCharCodes)
-console.log(numCharCodes)
-console.log(specCharCodes)
